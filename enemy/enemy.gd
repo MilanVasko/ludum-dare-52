9ba86224a9b1947b6e69498e4177fb6abce12c9f
Milan Vaško <m.vasko2@gmail.com>
2023-01-07 16:37:07 +0100

    More or less working pathfinding for enemy

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,4 +1,27 @@
-extends Node2D
+extends KinematicBody2D
+
+export(float) var speed := 190.0
+
+const threshold := 1.5
+
+var points := []
 
 func _ready() -> void:
 	$AnimationPlayer.play("rest")
+
+func _physics_process(delta: float) -> void:
+	#Navigation2DServer.map_get_path(, global_position, get_node("../Player").global_position, false)
+	points = get_node("../Navigation2D").get_simple_path(global_position, get_node("../Player").global_position, false)
+	if points.size() > 1:
+		var distance = points[1] - global_position
+		var direction = distance.normalized()
+		if distance.length() > threshold || points.size() > 2:
+			move_and_slide(direction * speed)
+		else:
+			move_and_slide(Vector2(0, 0))
+		update()
+
+func _draw() -> void:
+	if points.size() > 1:
+		for p in points:
+			draw_circle(p - global_position, 8, Color(1, 0, 0))
