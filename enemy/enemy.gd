d6252f45035e0dfe33e3d74ee7de67f169db52e0
Milan Vaško <m.vasko2@gmail.com>
2023-01-07 18:23:38 +0100

    Fix warnings

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -9,15 +9,15 @@ var points := []
 func _ready() -> void:
 	$AnimationPlayer.play("rest")
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	points = get_node("/root/Game/Navigation2D").get_simple_path(global_position, get_node("/root/Game/Entities/Player").global_position, true)
 	if points.size() > 1:
 		var distance = points[1] - global_position
 		var direction = distance.normalized()
 		if distance.length() > threshold || points.size() > 2:
-			move_and_slide(direction * speed)
+			var _ignored := move_and_slide(direction * speed)
 		else:
-			move_and_slide(Vector2(0, 0))
+			var _ignored := move_and_slide(Vector2(0, 0))
 		update()
 
 func _draw() -> void:
