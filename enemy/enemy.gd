9e9fd8cd1fbccfe4cc03acf8adfc9d5c09cef7ae
Milan Vaško <m.vasko2@gmail.com>
2023-01-08 23:22:59 +0100

    The enemy now follows the player slowly if he loses track of him

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -5,6 +5,7 @@ onready var last_known_player_position := global_position
 
 const threshold := 1.5
 var player_follow_delay := -1.0
+var wait_time := Global.enemy_wait_time
 
 var points := []
 var ray := Vector2.ZERO
@@ -36,19 +37,34 @@ func _physics_process(delta: float) -> void:
 	if can_see_player():
 		last_known_player_position = player_node.global_position
 		player_follow_delay = Global.enemy_player_follow_delay
+		wait_time = Global.enemy_wait_time
+		var _ignored := go_towards(last_known_player_position, Global.enemy_run_speed)
 	elif player_follow_delay > 0.0:
 		last_known_player_position = player_node.global_position
 		player_follow_delay -= delta
+		var _ignored := go_towards(last_known_player_position, Global.enemy_run_speed)
+	else:
+		if wait_time > 0.0:
+			if go_towards(last_known_player_position, Global.enemy_run_speed):
+				wait_time -= delta
+				return
+		else:
+			var _ignored := go_towards(player_node.global_position, Global.enemy_walk_speed)
 
-	points = find_navigation().get_simple_path(global_position, last_known_player_position, true)
+func go_towards(target_position: Vector2, speed: float) -> bool:
+	var is_idle := true
+	points = find_navigation().get_simple_path(global_position, target_position, true)
+
 	if points.size() > 1:
 		var distance = points[1] - global_position
 		var direction = distance.normalized()
 		if distance.length() > threshold || points.size() > 2:
-			var _ignored := move_and_slide(direction * Global.enemy_run_speed)
+			var _ignored := move_and_slide(direction * speed)
+			is_idle = false
 		else:
 			var _ignored := move_and_slide(Vector2(0, 0))
 		update()
+	return is_idle
 
 func _draw() -> void:
 	if points.size() > 1:
