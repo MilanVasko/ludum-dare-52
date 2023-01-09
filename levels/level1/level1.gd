996bb962554b86a2866accbdac91cd047caa9ecf
Milan Vaško <m.vasko2@gmail.com>
2023-01-09 15:45:15 +0100

    Fix warnings

--- levels/level1/level1.gd
+++ levels/level1/level1.gd
@@ -9,7 +9,7 @@ var burner_used := false
 func _can_use_burner() -> bool:
 	return !burner_used
 
-func _use_burner(burner: Node2D, caller: Node2D) -> void:
+func _use_burner(_burner: Node2D, _caller: Node2D) -> void:
 	var enemy := $Entities/Enemy
 	if !enemy.is_activated():
 		get_tree().call_group("dialogue_subscriber", "_on_dialogue_start", "level1/burner_intro")
