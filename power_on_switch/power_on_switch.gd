c231174c6f5f108b2e44a4c8c85edd644b302248
Milan Vaško <m.vasko2@gmail.com>
2023-01-28 11:18:58 +0100

    Refactor power on switch

--- power_on_switch/power_on_switch.gd
+++ power_on_switch/power_on_switch.gd
@@ -10,7 +10,6 @@ func _use(_called: Node) -> void:
 	already_used = true
 	get_tree().call_group("power_subscriber", "_on_plug_power")
 	get_tree().call_group("popup_subscriber", "_on_popup_show", popup_id)
-	var power = get_node("/root/Game/Entities/PowerRoom/Power")
-	power.get_node("Off").visible = false
-	power.get_node("On").visible = true
+	$Off.visible = false
+	$On.visible = true
 	$Noise.play()
