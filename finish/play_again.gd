08b8451cd815335082f97dd53b08d2ce5b416bef
Milan Vaško <m.vasko2@gmail.com>
2023-01-28 14:55:37 +0100

    Reorganize directory structure

--- finish/play_again.gd
+++ finish/play_again.gd
@@ -1,5 +1,5 @@
 extends Button
 
 func _on_play_again_pressed() -> void:
-	var err := get_tree().change_scene("res://levels/level1/level1.tscn")
+	var err := get_tree().change_scene("res://level1/level1.tscn")
 	assert(err == OK)
